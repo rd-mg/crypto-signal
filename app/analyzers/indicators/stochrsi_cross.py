d0fb8f219b668685c45492ba57d8c0401c54fb3e
rdmachadog <Entrarya0218*->
2022-11-15 00:20:40 -0400

    stochrsi_cross changed to pandas-ta

--- app/analyzers/indicators/stochrsi_cross.py
+++ app/analyzers/indicators/stochrsi_cross.py
@@ -27,28 +27,23 @@ class StochRSICross(IndicatorUtils):
 
         dataframe = self.convert_to_dataframe(historical_data)
 
-        rsi = abstract.RSI(dataframe, period_count)
-        stochrsi  = (rsi - rsi.rolling(period_count).min()) / (rsi.rolling(period_count).max() - rsi.rolling(period_count).min())
-        stochrsi_K = stochrsi.rolling(smooth_k).mean()
-        stochrsi_D = stochrsi_K.rolling(smooth_d).mean()
+        df = pandas.DataFrame()
+        df = dataframe.copy()
+        df.ta.stochrsi(append=True)
 
-        kd_values = pandas.DataFrame([rsi, stochrsi, stochrsi_K, stochrsi_D]).T.rename(
-            columns={0: "rsi", 1: "stoch_rsi", 2: "smooth_k", 3: "smooth_d"}).copy()
+        df['stoch_rsi'] = df['STOCHRSIk_14_14_3_3']
+        df['smooth_k'] = df['STOCHRSIk_14_14_3_3']
+        df['smooth_d'] = df['STOCHRSId_14_14_3_3']
 
-        kd_values['stoch_rsi'] = kd_values['stoch_rsi'].multiply(100)
-        kd_values['smooth_k'] = kd_values['smooth_k'].multiply(100)
-        kd_values['smooth_d'] = kd_values['smooth_d'].multiply(100)
+        df.dropna(how='all', inplace=True)
+        
+        previous_k, previous_d = df.iloc[-2]['smooth_k'], df.iloc[-2]['smooth_d']
+        current_k, current_d = df.iloc[-1]['smooth_k'], df.iloc[-1]['smooth_d']
 
-        stoch_cross = pandas.concat([dataframe, kd_values], axis=1)
-        stoch_cross.dropna(how='all', inplace=True)
+        df['is_hot'] = False
+        df['is_cold'] = False
 
-        previous_k, previous_d = stoch_cross.iloc[-2]['smooth_k'], stoch_cross.iloc[-2]['smooth_d']
-        current_k, current_d = stoch_cross.iloc[-1]['smooth_k'], stoch_cross.iloc[-1]['smooth_d']
+        df.at[df.index[-1], 'is_cold'] = previous_k > previous_d and current_k < current_d
+        df.at[df.index[-1], 'is_hot'] = previous_k < previous_d and current_k > current_d
 
-        stoch_cross['is_hot'] = False
-        stoch_cross['is_cold'] = False
-
-        stoch_cross.at[stoch_cross.index[-1], 'is_cold'] = previous_k > previous_d and current_k < current_d
-        stoch_cross.at[stoch_cross.index[-1], 'is_hot'] = previous_k < previous_d and current_k > current_d
-
-        return stoch_cross
+        return df
